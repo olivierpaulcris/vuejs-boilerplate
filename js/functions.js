1014176609db4de622b9c7b5c4f95c2937fea1e2
Paul Cristerna <olivierpaulcristerna@gmail.com>
2020-12-03 12:51:48 -0700

    v0.0.1: Se agregan archivos iniciales.

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+const app = new Vue({
+     el: '#app',
+     data: {
+          title: 'Hello World using Vuejs',
+          fruits: [{
+                    name: 'Apple',
+                    quantity: 3
+               },
+               {
+                    name: 'Orange',
+                    quantity: 0
+               },
+               {
+                    name: 'Pear',
+                    quantity: 6
+               },
+               {
+                    name: 'Bannana',
+                    quantity: 8
+               }
+          ]
+     }
+});
